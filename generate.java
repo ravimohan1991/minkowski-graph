41825dba46a289284b26433aa230b63fa6fb3787
ravimohan1991 <ravimohan1991@gmail.com>
2015-07-08 13:53:26 +0530

    License added

--- generate.java
+++ generate.java
@@ -1,13 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *  This program is distributed under the terms of the GNU General Public License
+ *  Copyright 2015, Ravi Mohan
+ */
+
+
+/**  
+ *  This file is part of minkowski-graph.
+ *
+ *  minkowski-graph is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  minkowski-graph is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with minkowski-graph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  * $version - 0.9
  * $author - The_Cowboy
  * $date - July 7, 2015
+ * $purpose - generate the data points for spacetime plots and store in txt files
  */
 
 import java.util.Scanner;
